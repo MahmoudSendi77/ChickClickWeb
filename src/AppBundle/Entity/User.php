54a15a99bbb27407d3a18c633508cb4684f61422
mahmoudsendi77 <mahmoudsendi77@gmail.com>
2019-11-21 22:07:13 +0100

    template and fos user

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahmoud
+ * Date: 21-Nov-19
+ * Time: 10:53
+ */
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
---
+++ src/Mahmoud/EventBundle/Controller/HomeEventController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Mahmoud\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Mahmoud\EventBundle\Entity\Event;
+
+
+class HomeEventController extends Controller
+{
+    public function homeEventAction()
+    {
+        return $this->render('@MahmoudEvent/HomeEvent/home_event.html.twig', array(
+            // ...
+        ));
+    }
+
+
+    public function showEventAction()
+    {
+
+        $rep=$this->getDoctrine()->getManager()->getRepository(Event::class)->findAll();
+//        $images = array();
+//        foreach ($rep as $key => $entity) {
+//            $images[$key] = base64_encode(stream_get_contents($entity->getEventPicture()));
+//        }
+
+        return $this->render('@MahmoudEvent/HomeEvent/show_event.html.twig', array(
+           'listEvent'=>$rep,
+
+        ));
+    }
+
+    public function addEventAction()
+    {
+        $e = new Event();
+        $rep=$this->getDoctrine()->getManager()->getRepository('EventBundle:Event')->findAll();
+
+        return $this->render('@MahmoudEvent/HomeEvent/show_event.html.twig', array(
+            'listEvent'=>$rep
+        ));
+    }
+    public function deleteEventAction()
+    {
+        $e = new Event();
+        $rep=$this->getDoctrine()->getRepository('EventBundle:Event')->findAll();
+
+        return $this->render('@MahmoudEvent/HomeEvent/show_event.html.twig', array(
+            'listEvent'=>$rep
+        ));
+    }
+
+}
